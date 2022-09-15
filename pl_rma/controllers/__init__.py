b4b2f7e2d90e0c05c613b6fcee721bb8536ad115
Yangtze <along008@gmail.com>
2022-09-15 08:39:16 -0700

    add

---
+++ pl_rma/controllers/__init__.py
@@ -0,0 +1,4 @@
+# Part of PLSuite Professional. See LICENSE_PROFESSIONAL file for full copyright and licensing details.
+
+from . import main
+from . import portal
---
+++ pl_rma/models/__init__.py
@@ -0,0 +1,5 @@
+# Part of PLSuite Professional. See LICENSE_PROFESSIONAL file for full copyright and licensing details.
+
+from . import account
+from . import rma
+from . import stock_picking
